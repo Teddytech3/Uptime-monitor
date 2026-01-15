937bc5cbea0365a1c682aca8c995149acd6adcd2
Kibetfx1 <kibetwycliffe093@gmail.com>
2026-01-15 20:05:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
-const urls = (process.env.RENDER_APP_URL || "")
+const urls = (process.env.RENDER_APP_URL || "https://teddy-xmdmini.onrender.com")
   .split(",")
   .map(u => u.trim())
   .filter(u => u.length > 0);
